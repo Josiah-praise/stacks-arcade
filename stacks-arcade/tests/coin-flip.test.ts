5739c96ded83d36afecd0095fea585ccd359587c
Josiah Praise <praisechinonso21@gmail.com>
2025-12-10 16:19:31 +0100

    tests: reject invalid coin pick

--- stacks-arcade/tests/coin-flip.test.ts
+++ stacks-arcade/tests/coin-flip.test.ts
@@ -45,6 +45,16 @@ describe("coin-flip", () => {
     expect(result).toBeErr();
   });
 
+  it("rejects invalid picks", () => {
+    const { result } = simnet.callPublicFn(
+      "coin-flip",
+      "create-game",
+      [simnet.uint(1_000_000n), simnet.uint(2)],
+      address1
+    );
+    expect(result).toBeErr();
+  });
+
   // it("shows an example", () => {
   //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
   //   expect(result).toBeUint(0);
