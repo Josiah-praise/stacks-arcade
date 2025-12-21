72902a36d404ae86861e3f82b020106811a4845c
Biokes <106444765+Biokes@users.noreply.github.com>
2025-12-21 22:26:28 +0100

    added contract correct path

---
+++ stacks-arcade/deployments/coin-flip.ts
@@ -0,0 +1,32 @@
+import { 
+  makeContractDeploy,
+  broadcastTransaction,
+  AnchorMode 
+} from '@stacks/transactions';
+import { STACKS_TESTNET } from '@stacks/network';
+import { readFileSync } from 'fs';
+import { CONFIG } from './config.ts';
+
+async function deployContract() {
+  const network = STACKS_TESTNET;
+  const contractSource = readFileSync('./contracts/coin-flip.clar', 'utf-8');
+  
+  const txOptions = {
+    contractName: 'coin-flip',
+    codeBody: contractSource,
+    senderKey: CONFIG.PRIVATE_KEY,
+    network,
+    anchorMode: AnchorMode.Any,
+    fee: 10000,
+  };
+  
+  const transaction = await makeContractDeploy(txOptions);
+  console.log("Transaction: ", transaction)
+  // const broadcastResponse = await broadcastTransaction(transaction, network);
+  
+  // console.log('Contract deployed!');
+  // console.log('Transaction ID:', broadcastResponse.txid);
+  // console.log('Contract address:', `${senderAddress}.${txOptions.contractName}`);
+}
+
+await deployContract();
